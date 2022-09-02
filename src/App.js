bf78fc15bb9f940cb89865e4ec77f7d992342af3
Brendan O'Connor <bhoconnor@gmail.com>
2022-09-02 15:50:30 -0400

    Move list item into new TodoListItem component, handle form submission, & use state for new to dos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+// APP COMPONENT / FUNCTION//////////////////////////////////////////////////////
 function App() {
+  // create a new state variable named `newTodo` with update function named `setNewTodo` (with useState hook)
+  const [newTodo, setNewTodo] = React.useState("");
+
   return (
     <div style={{ textAlign: "center" }}>
       <header>
         <h1>To do list</h1>
-        <AddTodoForm />
+        {/* pass `setNewTodo` as a callback handler prop named `onAddTodo` to `AddTodoForm` component */}
+        <AddTodoForm onAddTodo={setNewTodo} />
+        {/* paragraph element that displays value of `newTodo` variable */}
+        <p>{newTodo}</p>
         <TodoList />
       </header>
     </div>
---
+++ src/TodoListItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//TO DO LIST ITEM FORM COMPONENT / FUNCTION//////////////////////////////////////////////////////
+const TodoListItem = (props) => {
+  return <li> {props.item.title} </li>;
+};
+
+export default TodoListItem;
