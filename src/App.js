4273c2ce40560271a72e0eed1f265eb024a907e7
Brendan O'Connor <bhoconnor@gmail.com>
2023-02-18 19:01:00 -0500

    Remove useSemiPersistentState,

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // {useState} below allows to avoid typing "React.useState" when creating a variable
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
@@ -19,36 +19,66 @@ import AddTodoForm from "./AddTodoForm";
 //   // },
 // ];
 
-// savedTodoListToParse is to feed into JSON.parse as the default todoList useState value.
+// ************************************************************************************************************************ //
+// CUSTOM HOOK (useSemiPersistentState) ///////////////////////////////////////////////////////////////
+// ************************************************************************************************************************ //
+
+// savedTodoListToParse was to feed into JSON.parse as the default todoList useState value, changed in 1-7 to feed into mimicking of async data fetching via useEffect hook in App component.
 const savedTodoListToParse = localStorage.getItem("savedTodoList");
 
-// Instructions from 1-5: "Above the `App` functional component, create a new function named `useSemiPersistentState` which will be a custom hook"
-const useSemiPersistentState = () => {
-  // REMOVED BELOW newTodo STATE VARIABLE FOR LESSON 1-4:
-  // Below sets new to do's. Creates a new state variable named `newTodo` with update function named `setNewTodo` (with useState hook)
-  // const [newTodo, setNewTodo] = useState("");
+// // New function named `useSemiPersistentState`, a custom hook. (Lesson 1-5)
+// const useSemiPersistentState = () => {
+//   // REMOVED BELOW newTodo STATE VARIABLE FOR LESSON 1-4 & MOVED useState & useEffect FUNCTIONS INTO APP COMPONENT FOR 1-7
 
-  // Below stores ALL to do's. Creates new state variable called "todoList" w/"setter" called "setTodoList" w/an empty array as default value (was "todos" prior to that, but then had recommended to change to empty array). This was "lifting state" step.
-  const [todoList, setTodoList] = useState(
-    // Instructions: "Update the default state for `todoList` to read your `"savedTodoList"` item from `localStorage`. Then update your default state to parse the value of the `"savedTodoList"` item. Also had to add or statement || followed by empty bracket, so when the local storage is cleared & therefore empty, it still works.
-    JSON.parse(savedTodoListToParse) || []
-  );
+//   // Below sets new to do's. Creates a new state variable named `newTodo` with update function named `setNewTodo` (with useState hook)
 
-  // Instructions from 1-5: "Define a `useEffect` React hook with `todoList` as a dependency; inside the side-effect handler function, save the `todoList` inside `localStorage` with the key `"savedTodoList"` "
-  React.useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
+//   // const [newTodo, setNewTodo] = useState("");
 
-  // Instructions from 1-5: "Add a `return` statement in `useSemiPersistentState` that returns the `todoList` state variable and setter in an Array (just like how it's returned from the `useState` hook)"
-  return [todoList, setTodoList];
-};
+//   // Returns todoList state variable & state setter in an array (like how it's returned from a useState hook, Lesson 1-5)
+//   return [todoList, setTodoList];
+// };
 
 // ************************************************************************************************************************ //
 // APP COMPONENT / FUNCTION//////////////////////////////////////////////////////
 // ************************************************************************************************************************ //
 function App() {
-  //  "Update `App` to use new custom hook. Hint: Copy `useState` hook from before, but change `useState` to custom hook `useSemiPersistentState` (no arguments)" (Lesson 1-5)
-  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList");
+  //  Commented out useSemiPersistentState b/c moved to async data. Previously had updated App to use it. Copied useState hook from before, but changde useState to useSemiPersistentState w/out any arguments. (Lesson 1-5)
+  // const [todoList, setTodoList] = useSemiPersistentState("savedTodoList");
+
+  // ************************************************************************************************************************ //
+  // STATE: Various situations dealing with State below
+
+  // Below stores ALL to do's. Creates new state variable called "todoList" w/"setter" called "setTodoList" w/an empty array as default value (was "todos" prior to that, but then had recommended to change to empty array). This was previously a part of "lifting state" step.
+  const [todoList, setTodoList] = useState(
+    // Updated  default state for todoList to read "savedTodoList" item from localStorage. Then updated default state to parse  value of the "savedTodoList" item. Also had to add or statement || followed by empty bracket, so when local storage is cleared & therefore empty, it still works.
+    []
+  );
+
+  // For dealing w/State when data is loading
+  const [isLoading, setIsLoading] = useState(true);
+
+  // // 1st useEffect hook, to mimic async data fetching & to take saved todoList items (in stringified format), parse them into JS objects, & add those objects as an array to the setTodolist state updater function (1-7).
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(savedTodoListToParse) || [],
+          },
+        });
+      }, 2000);
+    }).then((result) => {
+      setTodoList([...result.data.todoList]);
+      setIsLoading(false);
+    });
+  }, []);
+
+  // 2nd useEffect hook, defined with todoList as a dependency; inside the side-effect handler function, saved todoList inside localStorage with the key "savedTodoList" (Lesson 1-5); changed so only sets localStorage if isLoading is false (1-7).
+  useEffect(() => {
+    if (isLoading === false) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
+  }, [todoList]);
 
   // "Declare new function named `addTodo` that takes `newTodo` as parameter. Call `setTodoList` state setter & use spread operator to pass existing Objects in `todoList` Array along with `newTodo` Object." (Lesson 1-4)
   const addTodo = (newTodo) => {
@@ -74,11 +104,15 @@ function App() {
         {/* Instantiation of AddTodoForm */}
         {/* "Change the value of the `onAddTodo` prop for `AddTodoForm` to `addTodo`" (Lesson 1-4) */}
         <AddTodoForm onAddTodo={addTodo} />
-        {/* Paragraph element that displays value of `newTodo` variable (Lesson 1-4) */}
+        {/* Paragraph to display value of `newTodo` variable (Lesson 1-4) */}
         {/* <p>{newTodo}</p> */}
-        {/* ************************************************************************************************************************ */}
-        {/* Instantiation of TodoList */}
-        <TodoList todos={todoList} onRemoveTodo={removeTodo} />
+        {isLoading ? (
+          <p style={{ textAlign: "left" }}>Loading...</p>
+        ) : (
+          // ************************************************************************************************************************
+          // Instantiation of TodoList
+          <TodoList todos={todoList} onRemoveTodo={removeTodo} />
+        )}
       </header>
     </>
   );
